6914b9aca43b629e2f26dfffceeb444c09b9544d
Javier Jimeno <javijimeza@gmail.com>
2017-03-06 11:07:17 +0100

    capturados todos los jugadores por orden alfabetico
    Se capturan todos los jugadores de la página por orden alfabético. Falta
    capturar el país.

--- captura_datos.py
+++ captura_datos.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import pandas as pd
+import string
 
 url = 'http://www.bdbasket.com/es/index.html'
 req = requests.get(url)
@@ -17,4 +18,32 @@ for j in lista_a:
      link= j.get('href')
      if str(link).find("t/t2") != -1 or str(link).find("t/t1") != -1:
          lista_url_temp.append('http://www.bdbasket.com/es/' +link)
-print lista_url_temp
+#print lista_url_temp
+
+def lectura_jugadores(url, lista_jugadores):
+    try:
+        tables = pd.read_html(url, header=0)
+        print len(tables[1])
+#        print (tables[1][1])
+        if len(tables[1]) > 0:  
+            lista_jugadores.append(tables[1])
+            return lista_jugadores
+    except:
+        return lista_jugadores
+        print 'no se ha podido leer la página %s'%url
+jugadores = ''
+lista_jugadores = []
+for j in range(0,26):
+    letra = string.ascii_lowercase[j]
+    url = 'http://www.bdbasket.com/es/a/j_' + letra + '.html'
+    for i in range(1,3):
+        if i == 1:
+            url = url
+        else:
+            url = url[:-6] + str(i) + url[-5:]
+        print url
+        lista_jugadores= lectura_jugadores(url, lista_jugadores)
+        #print lista_jugadores
+    jugadores = pd.concat(lista_jugadores, ignore_index=True)
+    jugadores = jugadores.drop_duplicates()
+print len(jugadores)
