50667ad46dfa2c1c93ba3e90dd03458f9f761997
Javier Jimeno <javijimeza@gmail.com>
2017-05-30 17:55:46 +0200

    buscando modelo
    Desisto de la regresión lineal multiple. En el siguiente paso nos vamos
    a un Lasso (o)

--- modifico_df.py
+++ modifico_df.py
@@ -5,6 +5,7 @@
 # cojo el fichero y empiezo a hacer transformaciones y algo de ML
 import pandas as pd
 import os
+import statsmodels.formula.api as sm
 
 def leo_ini(ruta):
     if ruta != "":
@@ -23,16 +24,23 @@ ruta = leo_ini("D:\Master\ProyectoBasket\Proyecto-Kschool-Basket")
 df = pd.read_csv(os.path.join(ruta, "temporadas.csv"), sep = ',')
 print df.head()
 df["porcentaje_jug_esp"] = df["jug_esp"]/df["total_jug"]
-df["porcentaje_jug_1"] = df["jug_1"]/df["total_jug"]
-df["porcentaje_jug_3"] = df["jug_3"]/df["total_jug"]
-df["porcentaje_jug_5"] = df["jug_5"]/df["total_jug"]
+df["porcentaje_jug_1"] = df["jug1"]/df["total_jug"]
+df["porcentaje_jug_3"] = df["jug3"]/df["total_jug"]
+df["porcentaje_jug_5"] = df["jug5"]/df["total_jug"]
 df["porcentaje_jug_mas5"] = df["mas5"]/df["total_jug"]
 df["total_min_no_esp"] = df["total_min"]-df["total_min_espana"]
 df["porcentaje_min_esp"]= df["total_min_espana"]/df["total_min"]
-df["porcentaje_min__no_esp"]= df["total_min_no_esp"]/df["total_min"]
+df["porcentaje_min_no_esp"]= df["total_min_no_esp"]/df["total_min"]
 df["total_puntos_no_esp"] = df["total_puntos"]-df["total_puntos_espana"]
 df["porcentaje_puntos_esp"]= df["total_puntos_espana"]/df["total_puntos"]
-df["porcentaje_puntos__no_esp"]= df["total_puntos_no_esp"]/df["total_puntos"]
+df["porcentaje_puntos_no_esp"]= df["total_puntos_no_esp"]/df["total_puntos"]
 df["total_val_no_esp"] = df["total_val"]-df["total_val_espana"]
 df["porcentaje_val_esp"]= df["total_val_espana"]/df["total_val"]
 df["porcentaje_val__no_esp"]= df["total_val_no_esp"]/df["total_val"]
+df = df.round(decimals=1)
+result = sm.ols(formula="puesto ~ jug_esp + jug1 +jug3 + jug5 + mas5 + total_min_espana + total_min_no_esp + total_puntos + total_puntos_espana + total_puntos_no_esp", data=df).fit()
+print result.params
+result.summary()
+result2 = sm.ols(formula="puesto ~ jug_esp + jug1 +jug3 + jug5 + mas5 + total_min_espana + total_min_no_esp + total_puntos + total_puntos_espana + total_puntos_no_esp", data=df).fit()
+df.to_csv("D:\\Master\\ProyectoBasket\\Proyecto-Kschool-Basket\\temporadas_totalizado.csv", index=False)
+result3 = sm.ols(formula="puesto ~ jug1 +jug3 + jug5 + porcentaje_jug_esp + porcentaje_min_esp", data=df).fit()
