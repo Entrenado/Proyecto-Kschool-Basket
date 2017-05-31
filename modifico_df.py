bbb9df390b1b80ec05403e4804d09b6c5391a33a
Javier Jimeno <javijimeza@gmail.com>
2017-05-31 22:54:10 +0200

    modifico para leer el ini del current dir

--- modifico_df.py
+++ modifico_df.py
@@ -7,19 +7,17 @@ import pandas as pd
 import os
 import statsmodels.formula.api as sm
 
-def leo_ini(ruta):
-    if ruta != "":
-        ruta = ruta
-    else: 
-        ruta = "D:\Master\ProyectoBasket\Proyecto-Kschool-Basket"
-    f1 = open(os.path.join(ruta, "ini.txt"))
+def leo_ini():
+    import os
+    script_dir = os.path.dirname(__file__)
+    f1 = open(os.path.join(script_dir,"ini.txt"), 'r')
     lineas=f1.readlines()
     for k in lineas:
         if k.find("ruta") != -1:
             ruta_final = k.split("=")[1]
     return ruta_final
     
-ruta = leo_ini("D:\Master\ProyectoBasket\Proyecto-Kschool-Basket")   
+ruta = leo_ini()   
 
 df = pd.read_csv(os.path.join(ruta, "temporadas.csv"), sep = ',')
 print df.head()
@@ -44,3 +42,11 @@ result.summary()
 result2 = sm.ols(formula="puesto ~ jug_esp + jug1 +jug3 + jug5 + mas5 + total_min_espana + total_min_no_esp + total_puntos + total_puntos_espana + total_puntos_no_esp", data=df).fit()
 df.to_csv("D:\\Master\\ProyectoBasket\\Proyecto-Kschool-Basket\\temporadas_totalizado.csv", index=False)
 result3 = sm.ols(formula="puesto ~ jug1 +jug3 + jug5 + porcentaje_jug_esp + porcentaje_min_esp", data=df).fit()
+result3.summary()
+#
+# Como no parece que funcione especialmente bien la regresión vamos a trabajar sobre un método de clasificación
+# Al final los puestos irán del 1 al 26 en el peor de los casos. Tomaremos esos puestos como categorías
+#
+
+
+
