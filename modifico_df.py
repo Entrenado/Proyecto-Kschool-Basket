e24fd975f92be859be0daaadb56fd76eabf6d92a
Javier Jimeno <javijimeza@gmail.com>
2017-06-06 17:26:45 +0200

    Improving descriptions

--- modifico_df.py
+++ modifico_df.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-# cojo el fichero y empiezo a hacer transformaciones y algo de ML
+# Read the web scrapped file, transform same values and start to apply Machine Learning
 import pandas as pd
 import os
 import statsmodels.formula.api as sm
@@ -43,7 +43,7 @@ df["total_val_no_esp"] = df["total_val"]-df["total_val_espana"]
 df["porcentaje_val_esp"]= df["total_val_espana"]/df["total_val"]
 df["porcentaje_val__no_esp"]= df["total_val_no_esp"]/df["total_val"]
 
-# Vamos a echarle un ojo a los datos
+# Reviewing the data
 
 df.describe()
 
@@ -62,11 +62,10 @@ df.to_csv("D:\\Master\\ProyectoBasket\\Proyecto-Kschool-Basket\\temporadas_total
 result3 = sm.ols(formula="puesto ~ jug1 +jug3 + jug5 + porcentaje_jug_esp + porcentaje_min_esp", data=df).fit()
 result3.summary()
 #
-# Como no parece que funcione especialmente bien la regresión vamos a trabajar sobre un método de clasificación
-# Al final los puestos irán del 1 al 26 en el peor de los casos. Tomaremos esos puestos como categorías
+# It seems that ther regression doesn't work we'll try to build a classification method. Though the position are numbers we can understand it like categories with values from 1 to 26. 
 #
 
-#voy a escalar los valores, para ello me quedo con las variables numéricas en el data frame
+# Scaling the values , I'm going to select the numeric columns in the dataframe
 columnas_numericas = ["PJ", "PG", "PP", "PF", "PC", "total_min", "total_val", "total_puntos", \
         "total_min_espana","total_puntos_espana", "total_val_espana", "total_jug", "jug_esp",\
        "jug1", "jug3", "jug5", "mas5", "porcentaje_jug_esp",\
@@ -79,12 +78,34 @@ columnas_numericas = ["PJ", "PG", "PP", "PF", "PC", "total_min", "total_val", "t
 df_numerico = df[columnas_numericas]
 clf = svm.SVC()
 df_numerico = df_numerico.apply(lambda x: preprocessing.scale(x))
-# añado la columna objetivo
+# adding target column 
 df_numerico["puesto"] =df["puesto"]
-# elimino PG, PP, puesto que son valores sobre los que no podemos hacer nada. Explican demasiado del puesto sin aportar valor
+# deleting PG and PP because we can't modify this values with people selection or behaviour. 
 df_numerico = df_numerico.drop(labels="PG", axis=1)
 df_numerico = df_numerico.drop(labels="PP", axis=1)
-# genero los conjuntos de train y test. Para validación se puede utilizar las temporadas que no están en los datos
+
+# Generating train and test subsets. If the accuracy is enough we'll use for control season that are not in the data (later 2016)
+X_train, X_test, y_train, y_test = train_test_split(df_numerico[jugadores],df_numerico["puesto"],test_size=0.2,random_state=0)
+C = 1.0  # SVM regularization parameter
+# Model creation with only players by years in the league. Must be interesting for the result
+svc = svm.SVC(kernel='linear', C=C).fit(X_train, y_train)
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X_train, y_train)
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X_train, y_train)
+lin_svc = svm.LinearSVC(C=C).fit(X_train, y_train)
+lista = [svc, rbf_svc,poly_svc,lin_svc]
+listado = ["svc", "rbf_svc","poly_svc","lin_svc"]
+contador = 0
+for k in lista:
+    clf_svm = k
+    clf_svm.fit(X_train, y_train)
+    y_pred_svm = clf_svm.predict(X_test)
+    acc_svm = accuracy_score(y_test, y_pred_svm)
+    print "accuracy %s: "%listado[contador],acc_svm
+    contador = contador +1
+
+#accuraccy is completely inefficient 
+
+# Model building adding other variables
 
 valores = ["PF", "PC", "total_min", "total_val","jug1", "jug3", "jug5", "mas5","total_jug","total_val_no_esp", "porcentaje_val_esp", "total_min_no_esp", "porcentaje_min_esp"]
 X_train, X_test, y_train, y_test = train_test_split(df_numerico[valores],df_numerico["puesto"],test_size=0.2,random_state=0)
@@ -103,3 +124,6 @@ for k in lista:
     acc_svm = accuracy_score(y_test, y_pred_svm)
     print "accuracy %s: "%listado[contador],acc_svm
     contador = contador +1
+
+# Better accuracy but still far from acceptables values 
+
